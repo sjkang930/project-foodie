39aa52dde2b3bbe3170867f47f422294a63a8e32
Gwanggyo Oh <gwanggyooh@Gwanggyoui-MacBookPro.local>
2022-07-23 16:23:18 -0700

    post page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Chat from './pages/Chat';
+import Comment from './pages/Comment';
+import Follow from './pages/Follow';
+import Map from './pages/Map';
+import New from './pages/New';
+import Home from './pages/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>
+          <a href="/"><img src='https://ifh.cc/g/9qtKfn.png' border='0' /></a>
+        </h2>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Chat" element={<Chat />} />
+          <Route path="/Comment" element={<Comment />} />
+          <Route path="/Follow" element={<Follow />} />
+          <Route path="/Map" element={<Map />} />
+          <Route path="/New" element={<New />} />
+        </Routes>
+        <nav className='bottome-nav'>
+          <a class="nav-btn" href={"/"}>
+            <img class="nav-icon" src="/icons/home.svg" />
+          </a>
+
+          <a class="nav-btn" href={"/Map"}>
+            <img class="nav-icon" src="icons/locationicon.svg" />
+          </a>
+
+          <a class="nav-btn" href={"/New"}>
+            <img class="nav-icon" src="icons/post-new.svg" />
+          </a>
+
+          <a class="nav-btn" href={"/Chat"}>
+            <img class="nav-icon" src="icons/txt.svg" />
+          </a>
+
+          <a class="nav-btn" href={"/Follow"}>
+            <img class="nav-icon" src="icons/profile.svg" />
+          </a>
+
+        </nav>
+      </div>
+    </BrowserRouter>
   );
 }
 

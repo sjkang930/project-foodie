22f86c8c4d5f65e0679bae49a86e41d18142c2a5
Gwanggyo Oh <gwanggyooh@Gwanggyoui-MacBookPro.local>
2022-07-25 16:59:20 -0700

    post page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
 import Comment from './components/Comment';
 import PostItem from './components/PostItem';
 import NavBar from './components/NavBar';
@@ -21,14 +21,29 @@ function App() {
         </h2>
         <Routes>
           <Route path="/" element={<Post />} />
-          <Route path="/Chat" element={<Chat />} />
-          <Route path="/Comment" element={<Comment />} />
-          <Route path="/Profile" element={<Profile />} />
-          <Route path="/Map" element={<Map />} />
-          <Route path="/Create" element={<Create />} />
+          <Route path="Map" element={<Map />} />
+          <Route path="Create" element={<Create />} />
+          <Route path="Chat" element={<Chat />} />
+          <Route path="Profile" element={<Profile />} />
         </Routes>
         <div>
-          <NavBar />
+          <nav>
+            <NavLink to="/">
+              <img class="nav-icon" src="/icons/home.svg" />
+            </NavLink>
+            <NavLink to="Map">
+              <img class="nav-icon" src="icons/locationicon.svg" />
+            </NavLink>
+            <NavLink to="Create">
+              <img class="nav-icon" src="icons/post-new.svg" />
+            </NavLink>
+            <NavLink to="Chat">
+              <img class="nav-icon" src="icons/txt.svg" />
+            </NavLink>
+            <NavLink to="Profile">
+              <img class="nav-icon" src="icons/profile.svg" />
+            </NavLink>
+          </nav>
         </div>
       </div>
     </BrowserRouter>
--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const Post = () => {
+    const [comment, setComment] = useState("");
     return (
         <div className="post">homepage
             <div className="post_head">
@@ -15,7 +18,7 @@ const Post = () => {
                 </div>
                 <div className="right_col">
                     <div className="more_info">
-                        <img src="/icons/png.png"/>
+                        <img src="/icons/png.png" />
                     </div>
                 </div>
 
@@ -24,16 +27,28 @@ const Post = () => {
 
             </div>
             <div className="left_icons">
-
+                <img src="/icons/heart.svg" />
+                <img src="/icons/message.svg" />
+                <img src="/icons/share.svg" />
             </div>
             <div className="right_icon">
-
+                <img src="/icons/map.svg" />
+            </div>
+            <div className="user_name">
+                suji kang
             </div>
             <div className="description">
-
+                sooooo goooood
             </div>
             <div className="comments">
-
+                <div className="total_comments">
+                    see all 0 comments
+                </div>
+                <input
+                    className="input_comments"
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                    type="text" />
             </div>
         </div>
     );

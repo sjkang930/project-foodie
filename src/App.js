a3646651290a10fca0cf47f565790fb13437ff64
Gwanggyo Oh <gwanggyooh@Gwanggyoui-MacBookPro.local>
2022-07-25 16:09:19 -0700

    for pull request

--- src/App.js
+++ src/App.js
@@ -1,54 +1,37 @@
+import React from 'react'
 import './App.css';
-import { BrowserRouter, Route, Routes, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Comment from './components/Comment';
+import PostItem from './components/PostItem';
+import NavBar from './components/NavBar';
+import Profile from './pages/Profile';
 import Chat from './pages/Chat';
-import Comment from './pages/Comment';
-import Follow from './pages/Follow';
 import Map from './pages/Map';
-import New from './pages/New';
-import Home from './pages/Home';
-
+import Create from './pages/Create';
+import Post from './pages/Post';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
     <BrowserRouter>
-      <Link to="/Chat">Chat</Link>
       <div className="App">
         <h2>
           <a href="/"><img src='https://ifh.cc/g/9qtKfn.png' border='0' /></a>
         </h2>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Post />} />
           <Route path="/Chat" element={<Chat />} />
           <Route path="/Comment" element={<Comment />} />
-          <Route path="/Follow" element={<Follow />} />
+          <Route path="/Profile" element={<Profile />} />
           <Route path="/Map" element={<Map />} />
-          <Route path="/New" element={<New />} />
-
+          <Route path="/Create" element={<Create />} />
         </Routes>
-        <nav className='bottome-nav'>
-          <NavLink className="nav-btn" to="/">
-            <img className="nav-icon" src="/icons/home.svg" />
-          </NavLink>
-
-          <a className="nav-btn" href={"/Map"}>
-            <img className="nav-icon" src="icons/locationicon.svg" />
-          </a>
-
-          <a className="nav-btn" href={"/New"}>
-            <img className="nav-icon" src="icons/post-new.svg" />
-          </a>
-
-          <a className="nav-btn" href={"/Chat"} >
-            <img className="nav-icon" src="icons/txt.svg" />
-          </a >
-
-          <a className="nav-btn" href={"/Follow"} >
-            <img className="nav-icon" src="icons/profile.svg" />
-          </a >
-
-        </nav >
-      </div >
-    </BrowserRouter >
+        <div>
+          <NavBar />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 

2b24487fe744ff4671432199573394db41d58d4d
sjkang930 <91238446+sjkang930@users.noreply.github.com>
2022-08-07 21:47:31 -0700

    auto complete API

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,11 @@
 import React from 'react'
 import './App.css';
 import { BrowserRouter, Route, Routes, NavLink } from 'react-router-dom';
-import Comment from './components/Comment';
-import PostItem from './components/PostItem';
-import NavBar from './components/NavBar';
 import Profile from './pages/Profile';
 import Chat from './pages/Chat';
 import Map from './pages/Map';
 import Create from './pages/Create';
 import Post from './pages/Post';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-
 
 function App() {
   return (

4d6e399e224d8848fe40373082bcd92a574ad92a
Gwanggyo Oh <gwanggyooh@Gwanggyoui-MacBookPro.local>
2022-08-15 16:22:35 -0700

    maprender

--- Frontend/src/pages/Map.js
+++ Frontend/src/pages/Map.js
@@ -1,10 +1,43 @@
-import React from 'react';
-const Map = () => {
-    return (
-        <div className="Map">
-            <p>Map page</p>
+import React, { useState, useCallback } from 'react';
+import { GoogleMap, useJsApiLoader, useLoadScript } from '@react-google-maps/api';
+const containerStyle = {
+    width: '400px',
+    height: '400px'
+};
 
-        </div>
-    )
+const center = {
+    lat: 49.2827,
+    lng: -123.1207
+};
+const Map = () => {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyAcaYDEWjjYRwiUJACWEOHC_HA32gaO7k0"
+    })
+
+    const [map, setMap] = useState(null)
+
+    const onLoad = useCallback(function callback(map) {
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+        setMap(map)
+    }, [])
+
+    const onUnmount = useCallback(function callback(map) {
+        setMap(null)
+    }, [])
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={5}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+        >
+
+            <></>
+        </GoogleMap>
+    ) : <></>
 }
-export default Map;
+export default React.memo(Map)
